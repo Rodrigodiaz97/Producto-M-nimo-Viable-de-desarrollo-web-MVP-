7861baf929c770fe023a409b1acac08523845e30
Rodrigo Díaz <84878129+Rodrigodiaz97@users.noreply.github.com>
2023-03-05 18:36:32 -0600

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -1,5 +1,5 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;port=8889;dbname=Networking',
+$pdo = new PDO('mysql:host=localhost;port=8889;dbname=NombreDB',
     'fred', 'zap');
 //See the "errors" folder for details....
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
